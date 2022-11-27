6aaa3a2aaf2cf98cb29468ea71c04ee672848b26
Huy Tran <huytran21007@gmail.com>
2022-11-27 08:55:10 +0700

    push code moi nhat bai 8

--- index.js
+++ index.js
@@ -6,6 +6,61 @@ app.use(express.static("."));
 
 app.listen(8080);
 
-app.get("/demo", (req,res) => {
-   res.send('hello');
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+app.get("/demo", async (req, res) => {
+  let data = await prisma.user.findMany({
+    include: {
+      food_type: true,
+    },
+    where: {
+      food_name: {
+        contains: "w", // where food_name like %a%
+      },
+    },
+  });
+  res.send(data);
+});
+
+app.post("/createFood", async (req, res) => {
+  let { food_name, image, price, desc, type_id } = req,
+    body;
+
+  // create food
+  await prisma.food.create({
+    data: {
+      food_name,
+      image,
+      price,
+      desc,
+      type_id,
+    },
+  });
+
+  // update food
+  await prisma.food.update({
+    where: { food_id: 1 },
+    data: {
+      food_name,
+      image,
+      price,
+      desc,
+      type_id,
+    },
+  });
+
+  // delete food
+  await prisma.food.delete({
+    where: { food_id: 1 },
+    data: {
+      food_name,
+      image,
+      price,
+      desc,
+      type_id,
+    },
+  });
+
+  res.send(data);
 });
